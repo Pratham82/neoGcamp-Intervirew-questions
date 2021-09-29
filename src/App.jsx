a6e1bc8df99d3fd079d5a70e92777731ddd2bfe3
pratham82 <mali.prathamesh82@gmail.com>
2021-09-29 23:07:52 +0530

    feat: Added 4th question in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './index.css'
 // import QuestionOne from './pages/interview-questions/question_01'
 // import QuestionTwo from './pages/interview-questions/question_02'
-import QuestionThree from './pages/interview-questions/question_03'
+// import QuestionThree from './pages/interview-questions/question_03'
+import QuestionFour from './pages/interview-questions/question_04'
 
 function App() {
   return (
     <div className="App">
-      <QuestionThree />
+      <QuestionFour />
     </div>
   )
 }
