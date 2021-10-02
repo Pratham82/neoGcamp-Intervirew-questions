92296479e88eaf7ece66659ca4627adacc4f63f7
pratham82 <mali.prathamesh82@gmail.com>
2021-10-02 15:50:31 +0530

    feat: Added 6th question

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './index.css'
 // import QuestionOne from './pages/interview-questions/question_01'
 // import QuestionTwo from './pages/interview-questions/question_02'
 // import QuestionThree from './pages/interview-questions/question_03'
-import QuestionFour from './pages/interview-questions/question_04'
+// import QuestionFour from './pages/interview-questions/question_04'
+import QuestionSix from './pages/interview-questions/question_06'
 
 function App() {
   return (
     <div className="App">
-      <QuestionFour />
+      <QuestionSix />
     </div>
   )
 }
---
+++ src/pages/interview-questions/question_06.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import QuestionHeader from '../../components/question-header'
+import '../../index.css'
+
+const QuestionSix = () => {
+  const [isLoading, setLoading] = useState(false)
+  const questionText =
+    '6. Create a web app with a button loaded. Show a text loading... on a web app. However, hide it if I click on the button loaded.'
+  return (
+    <React.Fragment>
+      <QuestionHeader questionText={questionText} />
+      {isLoading ? <h1>Loading....</h1> : <h1>Loaded</h1>}
+      <button
+        onClick={() => setLoading(!isLoading)}
+        className={`${
+          !isLoading ? 'loadingTemplate loading' : 'loadingTemplate loaded'
+        }`}
+      >
+        {isLoading ? 'Stop loading' : 'Start loading'}
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default QuestionSix
