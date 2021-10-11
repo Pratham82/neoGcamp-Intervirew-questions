f9c40e1a6484ea1c045fe1c4d81aeffb9f32f4ca
pratham82 <mali.prathamesh82@gmail.com>
2021-10-11 23:01:23 +0530

    feat: solved 7th question

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './index.css'
 // import QuestionTwo from './pages/interview-questions/question_02'
 // import QuestionThree from './pages/interview-questions/question_03'
 // import QuestionFour from './pages/interview-questions/question_04'
-import QuestionSix from './pages/interview-questions/question_06'
+// import QuestionSix from './pages/interview-questions/question_06'
+import QuestionSeven from './pages/interview-questions/question_07'
 
 function App() {
   return (
     <div className="App">
-      <QuestionSix />
+      <QuestionSeven />
     </div>
   )
 }
